bdd9c40fa6f238004288411a71db55771fa1c2fb
Pablo Brasero <pablo@pablobm.com>
2014-11-22 19:48:45 +0000

    Microgrep in C

---
+++ microgrep_in_c/src/microgrep.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <regex.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+void scan_file(char *path) {
+  int i;
+
+  regex_t re;
+  int retcode;
+  size_t errbuf_size;
+  char *errbuf;
+
+  size_t filebuf_size, line_size;
+  char *filebuf, *line;
+  char curchar;
+  unsigned int line_i, read_count, last_newline_i;
+  FILE *file;
+
+  retcode = regcomp(&re, "[0-9][0-9]\\.[0-9]ms)", 0);
+  if (retcode != 0) {
+    errbuf_size = regerror(retcode, &re, NULL, REG_NOSUB);
+    errbuf = malloc(errbuf_size);
+    regerror(retcode, &re, errbuf, errbuf_size);
+    fprintf(stderr, "%s", errbuf);
+    free(errbuf);
+    return;
+  }
+
+  file = fopen(path, "r");
+  if (file == NULL) {
+    printf("%s\n", strerror(errno));
+    return;
+  }
+  filebuf_size = line_size = 512;
+  filebuf = malloc(filebuf_size);
+  if (filebuf == NULL) {
+    printf("Could not allocate filebuf (%zd bytes)\n", filebuf_size);
+    return;
+  }
+  line = malloc(line_size);
+  if (line == NULL) {
+    printf("Could not allocate line (%zd bytes)\n", line_size);
+    return;
+  }
+  line_i = 0;
+  last_newline_i = 0;
+
+  while ((read_count = fread(filebuf, sizeof *filebuf, filebuf_size, file))) {
+    for (i = 0; i < read_count; i++) {
+      curchar = filebuf[i];
+      if (curchar == '\n') {
+        line[line_i] = '\0';
+        retcode = regexec(&re, line, 0, NULL, 0);
+        if (retcode == 0) {
+          printf("%s\n", line);
+        }
+        line_i = 0;
+      } else {
+        if (line_i == line_size) {
+          line_size *= 2;
+          if ((line = realloc(line, line_size)) == NULL) {
+            printf("Could not reallocate line (%zd bytes)\n", line_size);
+            return;
+          }
+        }
+        line[line_i++] = curchar;
+      }
+    }
+  }
+
+  free(line);
+  free(filebuf);
+}
+
+int main(int argc, char **argv) {
+  int i;
+  for (i = 1; i < argc; i++) {
+    scan_file(argv[i]);
+  }
+}
+
