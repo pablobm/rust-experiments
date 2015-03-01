d435e4c71302b0e35173761f7124549c3e595cae
Pablo Brasero <pablo@pablobm.com>
2015-03-01 18:38:11 +0000

    Refactored as per http://stackoverflow.com/questions/27094788/delegating-the-creation-of-data-structures

--- microgrep/src/microgrep.rs
+++ microgrep/src/microgrep.rs
@@ -4,29 +4,41 @@ extern crate regex_macros;
 extern crate regex;
 
 use std::os;
-use std::io::BufferedReader;
-use std::io::File;
+use std::io::{BufferedReader, File, Lines};
 
 fn main() {
     for target in os::args().iter() {
-        scan_file(target);
+        scan_file(target.as_slice());
     }
 }
 
-fn scan_file(path_str: &String) {
-    let re = regex!(r"[0-9][0-9]\.[0-9]ms\)");
-    let path = Path::new(path_str.as_bytes());
-    let file = File::open(&path);
-    let mut reader = BufferedReader::new(file);
+struct FileLines {
+    source: BufferedReader<File>
+}
 
-    for line in reader.lines() {
-        let s;
+impl FileLines {
+    fn new(path_str: &str) -> FileLines {
+        let path = Path::new(path_str.as_bytes());
+        let file = File::open(&path).unwrap();
+        let reader = BufferedReader::new(file);
+        FileLines { source: reader }
+    }
+
+    fn lines(&mut self) -> Lines<BufferedReader<File>> {
+        self.source.lines()
+    }
+}
+
+fn scan_file(path_str: &str) {
+    let re = regex!(r"[0-9][0-9]\.[0-9]ms\)");
+    for line in FileLines::new(path_str).lines() {
         match line {
-            Ok(_s) => s = _s,
-            Err(_) => return,
-        }
-        if re.is_match(s.as_slice()) {
-            print!("{}", s);
+            Ok(s) => {
+                if re.is_match(s.as_slice()) {
+                    print!("{}", s);
+                }
+            }
+            Err(_) => return
         }
     }
 }
