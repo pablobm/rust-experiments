37eaf4a4f611c12c272b47e1e52b657c78945279
Pablo Brasero <pablo@new-bamboo.co.uk>
2015-03-06 13:45:17 +0000

    Updated do build on current Rust
    $ rustc --version
    rustc 1.0.0-nightly (3b3bb0e68 2015-03-04) (built 2015-03-04)

--- microgrep_in_rust/src/microgrep.rs
+++ microgrep_in_rust/src/microgrep.rs
@@ -1,44 +1,59 @@
-#![feature(phase)]
-#[phase(plugin)]
-extern crate regex_macros;
+#![feature(io)]
+#![feature(fs)]
+#![feature(core)]
+#![feature(path)]
+
+#![feature(plugin)]
+#![plugin(regex_macros)]
 extern crate regex;
 
-use std::os;
-use std::io::{BufferedReader, File, Lines};
+use std::io::BufReader;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    for target in os::args().iter() {
+    for target in std::env::args() {
         scan_file(target.as_slice());
     }
 }
 
 struct FileLines {
-    source: BufferedReader<File>
+    source: BufReader<File>,
 }
 
 impl FileLines {
     fn new(path_str: &str) -> FileLines {
-        let path = Path::new(path_str.as_bytes());
+        let path = std::path::Path::new(path_str);
         let file = File::open(&path).unwrap();
-        let reader = BufferedReader::new(file);
+        let reader = BufReader::new(file);
         FileLines { source: reader }
     }
+}
 
-    fn lines(&mut self) -> Lines<BufferedReader<File>> {
-        self.source.lines()
+impl Iterator for FileLines {
+    type Item = String;
+
+    fn next(&mut self) -> Option<<FileLines as Iterator>::Item> {
+        let mut buf = String::new();
+        match self.source.read_line(&mut buf) {
+            Ok(_) => {
+                if buf.is_empty() {
+                    None
+                }
+                else {
+                    Some(buf)
+                }
+            }
+            Err(_) => None,
+        }
     }
 }
 
 fn scan_file(path_str: &str) {
     let re = regex!(r"[0-9][0-9]\.[0-9]ms\)");
-    for line in FileLines::new(path_str).lines() {
-        match line {
-            Ok(s) => {
-                if re.is_match(s.as_slice()) {
-                    print!("{}", s);
-                }
-            }
-            Err(_) => return
+    for line in FileLines::new(path_str) {
+        if re.is_match(line.as_slice()) {
+            print!("{}", line);
         }
     }
 }
