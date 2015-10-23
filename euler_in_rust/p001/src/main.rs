d2669002882fb18a5db1bd06ad1817933af36529
Pablo Brasero <pablo@new-bamboo.co.uk>
2015-10-23 11:42:28 +0100

    Problem 1

--- euler_in_rust/p001/src/main.rs
+++ euler_in_rust/p001/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut acc = 0;
+    for x in 0..1000 {
+        if x % 3 == 0 || x % 5 == 0 {
+            acc += x;
+        }
+    }
+    print!("{}\n", acc);
 }
