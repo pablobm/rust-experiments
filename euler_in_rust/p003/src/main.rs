47014be1906b27ba0589bf66ba03611706d41343
Pablo Brasero <pablo@thoughtbot.com>
2015-12-15 09:55:47 +0000

    Reusable library

--- euler_in_rust/p003/src/main.rs
+++ euler_in_rust/p003/src/main.rs
@@ -1,3 +1,7 @@
+extern crate euler;
+
+use euler::prime_sieve::PrimeSieve;
+
 pub fn main() {
     let target = 600851475143;
     let mut sieve = PrimeSieve::new();
@@ -6,59 +10,5 @@ pub fn main() {
     match prime_factors.last() {
         Some(x) => println!("{}", x),
         None => println!("{} is prime!", target),
-    }
-}
-
-struct PrimeSieve {
-    ordered_known_primes: Vec<u64>,
-}
-
-impl PrimeSieve {
-    fn new() -> PrimeSieve {
-        PrimeSieve {
-            ordered_known_primes: vec![2],
-        }
-    }
-
-    fn factors_of(&mut self, target: u64) -> Vec<u64> {
-        let mut factors: Vec<u64> = Vec::new();
-        let target_sqrt = (target as f64).sqrt() as u64;
-        self.extend_known_primes_up_to(target_sqrt);
-        self.collect_factors_from_known_primes(&mut factors, target);
-        factors
-    }
-
-    fn extend_known_primes_up_to(&mut self, limit: u64) {
-        for candidate in self.largest_known_prime()+1..limit {
-            let candidate_sqrt = (candidate as f64).sqrt() as u64;
-            let mut is_prime = true;
-            for prime in self.ordered_known_primes.iter() {
-                if prime > &candidate_sqrt {
-                    break
-                }
-                else if candidate % prime == 0 {
-                    is_prime = false;
-                    break;
-                }
-            }
-            if is_prime {
-                self.ordered_known_primes.push(candidate);
-            }
-        }
-    }
-
-    fn collect_factors_from_known_primes(&self, factors: &mut Vec<u64>, target: u64) {
-        let target_sqrt = (target as f64).sqrt() as u64;
-        for prime in self.ordered_known_primes.iter() {
-            if prime > &target_sqrt {
-                break;
-            } else if target % prime == 0 {
-                factors.push(*prime);
-            }
-        }
-    }
-
-    fn largest_known_prime(&self) -> &u64 {
-        self.ordered_known_primes.last().unwrap()
     }
 }
