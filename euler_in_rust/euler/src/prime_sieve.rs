8c9369a79ba134f7f97ed5461b0c8ce50cd0658c
Pablo Brasero <pablo@thoughtbot.com>
2015-12-16 18:12:57 +0000

    Ranges don’t include the top limit

--- euler_in_rust/euler/src/prime_sieve.rs
+++ euler_in_rust/euler/src/prime_sieve.rs
@@ -18,6 +18,10 @@ impl PrimeSieve {
     /// ```
     /// use euler::prime_sieve::PrimeSieve;
     ///
+    /// let factors = PrimeSieve::new().factors_of(9);
+    /// assert_eq!(factors.len(), 1);
+    /// assert_eq!(factors[0], 3);
+    ///
     /// let factors = PrimeSieve::new().factors_of(45);
     /// assert_eq!(factors.len(), 2);
     /// assert_eq!(factors[0], 3);
@@ -42,7 +46,7 @@ impl PrimeSieve {
     }
 
     fn extend_known_primes_up_to(&mut self, limit: u64) {
-        for candidate in self.largest_known_prime()+1..limit {
+        for candidate in self.largest_known_prime()+1..limit+1 {
             let candidate_sqrt = (candidate as f64).sqrt() as u64;
             let mut is_prime = true;
             for prime in self.ordered_known_primes.iter() {
