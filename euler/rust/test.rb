ac141b20973a2f4d46f61e6a48d6b53e6c49d0d0
Pablo Brasero <pablo@pablobm.com>
2018-11-24 13:49:01 +0000

    Include support libraries in test suite

--- euler/rust/test.rb
+++ euler/rust/test.rb
@@ -11,16 +11,33 @@ SOLUTIONS = {
   '010' => 142913828922,
 }
 
-successes = 0
-failures = 0
-SOLUTIONS.each do |number, solution|
-  result = `cd p#{number} && cargo run`.split.last.to_i
-  if result == solution
-    successes += 1
-  else
-    failures += 1
+$successes = 0
+$failures = 0
+
+def test_problems
+  SOLUTIONS.each do |number, solution|
+    result = `cd p#{number} && cargo run`.split.last.to_i
+    if result == solution
+      $successes += 1
+    else
+      puts "FAILURE: expected #{solution}, but got #{result}"
+      $failures += 1
+    end
   end
 end
 
-prefix = failures == 0 ? "SUCCESS" : "FAILURE"
-puts "#{prefix}: #{successes} successes, #{failures} failures"
+def test_support_library
+  output = `cd euler && cargo test`
+  if $? == 0
+    $successes += 1
+  else
+    puts output
+    $failures += 1
+  end
+end
+
+test_support_library
+test_problems
+
+prefix = $failures == 0 ? "SUCCESS" : "FAILURE"
+puts "#{prefix}: #{$successes} successes, #{$failures} failures"
